e5712948ecd1fbef4ec9ec09fe54bbd85ef63796
hansophiad <hansophiad@gmail.com>
2021-04-24 22:03:28 +0200

    add toggle function

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,11 @@
-console.log("Hello you! Thanks for checking out my portfolio.")
+<script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
+<script src="js/scripts.js"></script>
+
+function menuToggle() {
+  var x = document.getElementById('myNavtoggle');
+  if (x.className === 'navtoggle') {
+    x.className += ' responsive';
+  } else {
+    x.className = 'navtoggle';
+  }
+}
