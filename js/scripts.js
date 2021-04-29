3340bf356d1bc8b7c7c58af9ef2f0c075be4c4e2
hansophiad <hansophiad@gmail.com>
2021-04-29 19:35:03 +0200

    update js scripts

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,8 @@
 function menuToggle() {
-  var x = document.getElementById('myNavtoggle');
-  if (x.className === 'navtoggle' && screen.width < 640) {
-    x.className += ' responsive';
-  } else {
-    x.className = 'navtoggle';
-  }
+var x = document.getElementById('myNavtoggle');
+if (x.className === 'navtoggle' && screen.width < 640) {
+x.className += ' responsive';
+} else {
+x.className = 'navtoggle';
+}
 }
