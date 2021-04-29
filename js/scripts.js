968c652cd1b3de733af57e513b290f88a6f688cb
hansophiad <hansophiad@gmail.com>
2021-04-29 19:56:06 +0200

    add spaces to js scripts

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,8 @@
-<!DOCTYPE html>
-
-function menuToggle() {
-var x = document.getElementById('myNavtoggle');
-if (x.className === 'navtoggle' && screen.width < 640) {
-x.className += ' responsive';
-} else {
-x.className = 'navtoggle';
-}
+function menuToggle () {
+  var x = document.getElementById ('myNavtoggle');
+  if (x.className === 'navtoggle' && screen.width < 640) {
+    x.className += ' responsive';
+  } else {
+    x.className = 'navtoggle';
+  }
 }
